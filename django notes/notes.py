fb89ae69d2c9b1c81ae85683f225cf9bd9a07c1a
vamsi <xyz@gmail.com>
2023-12-26 16:22:39 +0530

    django methods

---
+++ django notes/notes.py
@@ -0,0 +1,24 @@
+"""
+1> we need to check the application of python version
+2> we need to create a virtual environment
+3> after that we need ton install django on your system
+4> we need to install the project name 
+5> change the directory the project name
+6> after that we need to install tha one application
+7> we need to write a models
+8> we need to runserver
+"""
+"""
+1> python --version
+2> pip --version
+3> pip install virtualenv
+4> virtualenv venv
+5> .\venv\Scripts\activate
+6> pip install django
+7> django-admin startproject projectname
+8> cd projectname # chaning to the firstproject
+9> py manage.py startapp appname
+10> py manage.py makemigrations
+11> py manage.py migrate
+12> py manage.py runserver
+"""
